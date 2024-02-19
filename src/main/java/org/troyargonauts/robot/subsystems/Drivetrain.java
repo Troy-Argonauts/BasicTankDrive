6c20e2eb404e0f23338b3aa5f29eda23e444b950
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2024-02-19 14:10:05 -0500

    Changed talonFX cim to talonSRX

--- src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
@@ -1,5 +1,9 @@
 package org.troyargonauts.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.controls.StaticBrake;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -7,47 +11,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    private TalonFX m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
+    private TalonSRX m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
     private double rightEncoderValue, leftEncoderValue;
     public Drivetrain(){
-        m_FL_Motor = new TalonFX(4);
-        m_FR_Motor = new TalonFX(3);
-        m_BL_Motor = new TalonFX(2);
-        m_BR_Motor = new TalonFX(0);
+        m_FL_Motor = new TalonSRX(4);
+        m_FR_Motor = new TalonSRX(3);
+        m_BL_Motor = new TalonSRX(2);
+        m_BR_Motor = new TalonSRX(0);
 
-        m_FL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_FR_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
+        m_FL_Motor.setNeutralMode(NeutralMode.Brake);
+        m_FR_Motor.setNeutralMode(NeutralMode.Brake);
+        m_BL_Motor.setNeutralMode(NeutralMode.Brake);
+        m_BL_Motor.setNeutralMode(NeutralMode.Brake);
+
     }
 
     public void resetEndcoders(){
-        m_FL_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
-        m_FR_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
+        m_FL_Motor.set(TalonSRXControlMode.Position, 0);
+        m_BL_Motor.set(TalonSRXControlMode.Position, 0);
+        m_FR_Motor.set(TalonSRXControlMode.Position, 0);
+        m_BL_Motor.set(TalonSRXControlMode.Position, 0);
     }
 
     @Override
     public void periodic(){
-        rightEncoderValue = (m_FL_Motor.getPosition().getValueAsDouble() + m_BL_Motor.getPosition().getValueAsDouble())/2;
-        leftEncoderValue = (m_FR_Motor.getPosition().getValueAsDouble() + m_BR_Motor.getPosition().getValueAsDouble())/2;
+        rightEncoderValue = (m_FL_Motor.getSelectedSensorPosition() + m_BL_Motor.getSelectedSensorPosition())/2;
+        leftEncoderValue = (m_FR_Motor.getSelectedSensorPosition() + m_BR_Motor.getSelectedSensorPosition())/2;
 
         SmartDashboard.putNumber("RightSideEncoder", rightEncoderValue);
         SmartDashboard.putNumber("LeftSideEncoder", leftEncoderValue);
     }
 
     public void cheesyDrive(double speed, double turn, double nerf){
-        m_FL_Motor.set((speed + turn) * nerf);
-        m_BL_Motor.set((speed + turn) * nerf);
-        m_FR_Motor.set((speed - turn) * nerf);
-        m_BR_Motor.set((speed - turn) * nerf);
+        m_FL_Motor.set(TalonSRXControlMode.PercentOutput, (speed + turn) * nerf);
+        m_BL_Motor.set(TalonSRXControlMode.PercentOutput, (speed + turn) * nerf);
+        m_FR_Motor.set(TalonSRXControlMode.PercentOutput, (speed - turn) * nerf);
+        m_BR_Motor.set(TalonSRXControlMode.PercentOutput, (speed - turn) * nerf);
     }
 
     public void tankDrive(double leftJoyValue, double rightJoyValue, double nerf){
-        m_FL_Motor.set(leftJoyValue * nerf);
-        m_BL_Motor.set(leftJoyValue * nerf);
-        m_FR_Motor.set(rightJoyValue * nerf);
-        m_BR_Motor.set(rightJoyValue * nerf);
+        m_FL_Motor.set(TalonSRXControlMode.PercentOutput, leftJoyValue * nerf);
+        m_BL_Motor.set(TalonSRXControlMode.PercentOutput, leftJoyValue * nerf);
+        m_FR_Motor.set(TalonSRXControlMode.PercentOutput, rightJoyValue * nerf);
+        m_BR_Motor.set(TalonSRXControlMode.PercentOutput, rightJoyValue * nerf);
     }
 }
